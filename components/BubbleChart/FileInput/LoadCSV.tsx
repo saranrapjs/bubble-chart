6ac19f324bf40e3660d378e0c390d2f84eed9bea
Jeff Sisson <jeff@bigboy.us>
2022-06-13 23:57:48 -0400

    adding initial support for sheets api

--- components/BubbleChart/FileInput/LoadCSV.tsx
+++ components/BubbleChart/FileInput/LoadCSV.tsx
@@ -1,10 +1,76 @@
 import * as React from 'react'
+import type { DSVRowArray } from 'd3-dsv';
 import { useContext, useRef } from 'react'
 import { Button, Form } from 'react-bootstrap'
 import { WorkerDataContext } from '../data/WorkerDataProvider'
 import { FormatAction } from '../data/dataFormattingReducer'
 import DropdownWithFilter from '../../shared/components/DropdownWithFilter'
 import { pxToRem } from '../../shared/tokens/spacing'
+
+declare global {
+  interface Window {
+    tokenClient?: google.accounts.oauth2.TokenClient;
+  }
+}
+
+function toDSV(headers: string[], rows: string[][]): DSVRowArray<string> {
+  // @ts-expect-error
+  const res: DSVRowArray<string> = rows.map(row => 
+    Object.fromEntries(Object.values(headers).map((key, i) => [key, row[i]]))
+  );
+
+  res.columns = headers;
+
+  return res;
+}
+
+async function loadSheet(spreadsheetId: string) {
+  console.warn('loading shset', spreadsheetId)
+  let response;
+  try {
+    const spread = await gapi.client.sheets.spreadsheets.get({
+      spreadsheetId,
+    });
+    const sheet = spread.result?.sheets?.find(sh => sh.properties?.sheetId === 0);
+    if (!sheet) throw new Error('couldnt find default sheet');
+    // Fetch first 10 files
+    response = await gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId,
+      range: sheet?.properties?.title || '',
+    });
+  } catch (err) {
+    console.error(err);
+    return;
+  }
+  const range = response.result;
+  if (!range || !range.values || range.values.length == 0) {
+    return;
+  }
+  return toDSV(range.values[0], range.values.slice(1));
+}
+
+function onSheetsClick(sheetId: string) {
+  return new Promise<DSVRowArray<string>>(resolve => {
+    if (window.tokenClient) {
+      // @ts-expect-error
+      window.tokenClient.callback = async (resp) => {
+        if (resp.error !== undefined) {
+          throw (resp);
+        }
+        const result = await loadSheet(sheetId);
+        if (result) resolve(result);
+      };
+      if (gapi.client.getToken() === null) {
+        // Prompt the user to select a Google Account and ask for consent to share their data
+        // when establishing a new session.
+        window.tokenClient.requestAccessToken({ prompt: 'consent' });
+      } else {
+        // Skip display of account chooser and consent dialog for an existing session.
+        window.tokenClient.requestAccessToken({ prompt: '' });
+      }
+    }
+  })
+}
 
 const columnMapLabels: { [s: string]: string } = {
   uniqueIdentifier: 'Unique identifier',
@@ -22,6 +88,8 @@ const LoadCSV: React.FC<{
   const convertedCsv = workersData
 
   const inputRef = useRef<HTMLInputElement>(null)
+
+  const [maybeSheets, setSheets] = React.useState('');
 
   return (
     <>
@@ -57,6 +125,32 @@ const LoadCSV: React.FC<{
               }}
             >
               ...or load example data
+            </Button>
+          </Form.Label>
+          <Form.Label>
+            <Form.Control
+              size="sm"
+              className={'form-control'}
+              type="text"
+              value={maybeSheets}
+              onChange={e => {
+                setSheets(e.target.value);
+              }}
+            />
+            <Button
+              variant={'secondary'}
+              size="sm"
+              style={{ marginBottom: pxToRem(4), marginLeft: pxToRem(6) }}
+              onClick={async () => {
+                const sheetMatch = maybeSheets.match(/spreadsheets\/d\/(.*)\/edit/);
+                if (sheetMatch) {
+                  const res = await onSheetsClick(sheetMatch[1]);
+                  console.warn('res', res);
+                  dispatch({ type: FormatAction.LOAD_WORKERS_CSV, parsedData: res });                
+                }
+              }}
+            >
+              Load from Sheets
             </Button>
           </Form.Label>
         </Form.Group>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
@@ -6,7 +7,45 @@ import Layout from '../components/shared/components/Layout'
 import WorkerDataProvider from '../components/BubbleChart/data/WorkerDataProvider'
 import BubbleChart from '../components/BubbleChart/BubbleChart'
 
+function gapiLoaded() {
+  gapi.load('client', intializeGapiClient);
+}
+async function intializeGapiClient() {
+  await gapi.client.init({
+    apiKey: process.env.NEXT_PUBLIC_SHEETS_API_KEY || '',
+    discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+  });
+}
+
+declare global {
+  interface Window {
+    tokenClient?: google.accounts.oauth2.TokenClient;
+  }
+}
+
+
+function gisLoaded() {
+  window.tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: process.env.NEXT_PUBLIC_SHEETS_CLIENT_ID || '',
+    scope: 'https://www.googleapis.com/auth/spreadsheets.readonly',
+    callback: () => {},
+  });
+}
+
+const loadScript = (url: string, cb: () => void) => {
+  const script = document.createElement('script');
+  script.async = true;
+  script.defer = true;
+  script.src = url;
+  script.onload = cb;
+  document.body.appendChild(script);
+}
+
 const Home: NextPage = () => {
+  useEffect(() => {
+    loadScript('https://apis.google.com/js/api.js', gapiLoaded);
+    loadScript('https://accounts.google.com/gsi/client', gisLoaded);
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
